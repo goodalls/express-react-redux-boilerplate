286a1c54cc965ba10248f95a9dd221fac294448e
Jeff Goodall <goodalls@live.com>
2018-06-06 11:00:08 -0600

    updates to server.js file

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const database = require('knex')(configuration);
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use(express.static('public'))
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static('public'));
 
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'express-react-redux-boilerplate';
+
+app.get('/', (request, response) => response.send('Hello World!'));
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
