d80f87d465aec9c878270aef2b3f204e39683fa2
Jeff Goodall <goodalls@live.com>
2018-06-06 10:58:18 -0600

    added package scripts to run api and client

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const app = express();
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.json());
+app.use(express.static('public'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.set('port', process.env.PORT || 3001);
+app.locals.title = 'express-react-redux-boilerplate';
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
