133edc4dada7c6862335673925cf8f9936269d58
Jeff Goodall <goodalls@live.com>
2018-06-11 09:27:48 -0600

    Adding Test and Production to Knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,34 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/publications',
+    connection: 'postgres://localhost/**DB_NAME_HERE**',
     migrations: {
       directory: './src/db/migrations'
     },
     seeds: {
       directory: './src/db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/**TEST_DB_NAME_HERE**',
+    migrations: {
+      directory: './src/db/migrations'
+    },
+    seeds: {
+      directory: './src/db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './API/db/migrations'
+    },
+    seeds: {
+      directory: './API/db/seeds/dev'
     },
     useNullAsDefault: true
   }
